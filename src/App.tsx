5a9038b7194c1d97bec1ebbd01c48b76a6aa7782
Yudi <yudiyamada10@gmail.com>
2025-10-26 14:53:34 -0300

    add tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-
 function App() {
   return (
     <>
-      <Header />
       <h1>Hello World</h1>
-      <Footer />
     </>
   );
 }
