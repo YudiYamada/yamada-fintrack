f1983d4da55e6b8f89d9c55adfdfc7156b37ded5
Yudi <yudiyamada10@gmail.com>
2025-11-10 15:36:45 -0300

    feat: add optional data in formprops interface

--- src/types/components/form.d.ts
+++ src/types/components/form.d.ts
@@ -1,8 +1,8 @@
 export interface FormProps {
   firstName?: string;
   lastName?: string;
-  email: string;
-  password: string;
+  email?: string;
+  password?: string;
   passwordConfirmation?: string;
   terms?: boolean;
 }
