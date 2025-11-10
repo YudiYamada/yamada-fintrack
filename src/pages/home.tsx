b35275171984b9f87190b9637ac39f998d53ebfc
Yudi <yudiyamada10@gmail.com>
2025-11-10 19:39:00 -0300

    feat: add header component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router";
 
-import { Button } from "@/components/ui/button";
+import Header from "@/components/header";
 import { useAuthContext } from "@/contexts/auth";
 
 function HomePage() {
-  const { user, isInitializing, signOut } = useAuthContext();
+  const { user, isInitializing } = useAuthContext();
   if (isInitializing) return null;
   if (!user) {
     return <Navigate to={`/login`} />;
@@ -12,9 +12,7 @@ function HomePage() {
 
   return (
     <>
-      <h1>Olá {user.first_name}</h1>
-      <p>Welcome to the Home page</p>
-      <Button onClick={signOut}>Sair</Button>
+      <Header />
     </>
   );
 }
