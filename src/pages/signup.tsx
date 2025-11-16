fb65a35d7715070667dd5f3bfbd1987e637c4690
Yudi <yudiyamada10@gmail.com>
2025-11-16 18:58:26 -0300

    refactor: add signup form hook

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,4 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { Link, Navigate } from "react-router";
-import * as z from "zod";
 
 import PasswordInput from "@/components/password-input";
 import { Button } from "@/components/ui/button";
@@ -24,58 +21,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useAuthContext } from "@/contexts/auth";
+import { useSignupForm } from "@/forms/hooks/user";
 import type { FormProps } from "@/types/components/form";
-
-const signupSchema = z
-  .object({
-    firstName: z.string().trim().min(1, {
-      message: "O nome é obrigatório.",
-    }),
-    lastName: z.string().trim().min(1, {
-      message: "O sobrenome é obrigatório.",
-    }),
-    email: z
-      .email({
-        message: "O e-mail é inválido.",
-      })
-      .trim()
-      .min(1, {
-        message: "O e-mail é obrigatório.",
-      }),
-    password: z.string().trim().min(6, {
-      message: "A senha deve ter no mínimo 6 caracteres.",
-    }),
-    passwordConfirmation: z.string().trim().min(6, {
-      message: "A confirmação de senha é obrigatória.",
-    }),
-    terms: z.boolean().refine((value) => value === true, {
-      message: "Você precisa aceitar os termos.",
-    }),
-  })
-  .refine(
-    (data) => {
-      return data.password === data.passwordConfirmation;
-    },
-    {
-      message: "As senhas não coincidem.",
-      path: ["passwordConfirmation"],
-    }
-  );
 
 function SignupPage() {
   const { user, signup, isInitializing } = useAuthContext();
 
-  const form = useForm<z.infer<typeof signupSchema>>({
-    resolver: zodResolver(signupSchema),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      passwordConfirmation: "",
-      terms: false,
-    },
-  });
+  const form = useSignupForm();
 
   const handleSubmit = (data: FormProps) => signup(data);
 
