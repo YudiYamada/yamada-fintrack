8ee43589f594eab3a3e3bae5e4923232c7490780
Yudi <yudiyamada10@gmail.com>
2025-11-25 19:53:02 -0300

    feat: add delete button

--- src/api/services/transaction.ts
+++ src/api/services/transaction.ts
@@ -2,8 +2,16 @@ import queryString from "query-string";
 
 import { protectedApi } from "@/lib/axios";
 
+interface Transaction {
+  id: string; // O ID deve ser obrigatório no retorno
+  name: string;
+  date: string; // Geralmente string ISO 8601
+  amount: number;
+  type: "INCOME" | "EXPENSE" | string; // Exemplo de tipagem mais estrita para 'type'
+}
+
 export const TransactionService = {
-  create: async (input) => {
+  create: async (input: Transaction) => {
     const response = await protectedApi.post("/transactions/me", {
       name: input.name,
       date: input.date,
@@ -12,12 +20,12 @@ export const TransactionService = {
     });
     return response.data;
   },
-  getAll: async (input) => {
+  getAll: async (input: { from: string; to: string }) => {
     const query = queryString.stringify({ from: input.from, to: input.to });
     const response = await protectedApi.get(`/transactions/me?${query}`);
     return response.data;
   },
-  update: async (input) => {
+  update: async (input: Transaction) => {
     const response = await protectedApi.patch(`/transactions/me/${input.id}`, {
       name: input.name,
       date: input.date,
